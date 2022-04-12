92e00e204aa2d14f169d625899d64253ecbda3b3
georg-nikola <gn.pavlov96@gmail.com>
2022-04-12 16:44:29 +0300

    Add profile details, change_password and fix base.html

--- budget/budget/auth_app/urls.py
+++ budget/budget/auth_app/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.views.generic import RedirectView
 
-from budget.auth_app.views import UserLoginView, UserRegisterView
+from budget.auth_app.views import UserLoginView, UserRegisterView, ChangeUserPasswordView, ProfileDetailView, \
+    EditProfileView, DeleteProfileView
 
 urlpatterns = (
     path('login/', UserLoginView.as_view(), name='login'),
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('change-password/', ChangeUserPasswordView.as_view(), name='change password'),
+    path('password_change_done/', RedirectView.as_view(url=reverse_lazy('index')), name='password_change_done'),
+    path('<int:pk>/', ProfileDetailView.as_view(), name='profile details'),
+    path('edit/', EditProfileView.as_view(), name='edit profile'),
+    path('delete/', DeleteProfileView.as_view(), name='delete profile'),
+
 )
--- budget/budget/auth_app/views.py
+++ budget/budget/auth_app/views.py
@@ -4,6 +4,7 @@ from django.views import generic as views
 from django.contrib.auth import mixins as auth_mixins
 
 from budget.auth_app.forms import CreateProfileForm
+from budget.auth_app.models import Profile
 
 
 class UserRegisterView(views.CreateView):
@@ -20,3 +21,30 @@ class UserLoginView(auth_views.LoginView):
         if self.success_url:
             return self.success_url
         return super().get_success_url()
+
+
+class ProfileDetailView(views.DetailView, auth_mixins.LoginRequiredMixin):
+    model = Profile
+    template_name = 'auth_app/profile_details.html'
+    context_object_name = 'profile'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # self.object is a Profile
+        context.update({
+            'is_owner': self.object.user_id == self.request.user.id,
+        })
+
+        return context
+
+
+class ChangeUserPasswordView(auth_views.PasswordChangeView):
+    template_name = 'auth_app/change_password.html'
+
+
+class EditProfileView(auth_views.FormView):
+    pass
+
+
+class DeleteProfileView(auth_views.FormView):
+    pass
