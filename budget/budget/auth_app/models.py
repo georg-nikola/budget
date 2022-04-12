d4b50bf0c2d44e9675c0394c29d09020610d4851
georg-nikola <gn.pavlov96@gmail.com>
2022-04-12 11:37:45 +0300

    Create user manager, create user and profile model, add to settings.py default auth model

--- budget/budget/auth_app/models.py
+++ budget/budget/auth_app/models.py
@@ -1,3 +1,65 @@
+from django.contrib.auth import models as auth_models
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+from budget.auth_app.managers import BudgetUserManager
+from budget.common.validators import validate_only_letters
+
+
+class BudgetUser(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
+    USERNAME_MAX_LENGTH = 25
+
+    username = models.CharField(
+        max_length=USERNAME_MAX_LENGTH,
+        unique=True,
+    )
+
+    date_joined = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    is_staff = models.BooleanField(
+        default=False,
+    )
+
+    USERNAME_FIELD = 'username'
+
+    objects = BudgetUserManager()
+
+
+class Profile(models.Model):
+    FIRST_NAME_MIN_LENGTH = 2
+    FIRST_NAME_MAX_LENGTH = 30
+    LAST_NAME_MIN_LENGTH = 2
+    LAST_NAME_MAX_LENGTH = 30
+
+    first_name = models.CharField(
+        max_length=FIRST_NAME_MAX_LENGTH,
+        validators=(
+            MinLengthValidator(FIRST_NAME_MIN_LENGTH),
+            validate_only_letters,
+        )
+    )
+
+    last_name = models.CharField(
+        max_length=LAST_NAME_MAX_LENGTH,
+        validators=(
+            MinLengthValidator(LAST_NAME_MIN_LENGTH),
+            validate_only_letters,
+        )
+    )
+
+    email = models.EmailField(
+        null=True,
+        blank=True,
+    )
+
+    user = models.OneToOneField(
+        BudgetUser,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
