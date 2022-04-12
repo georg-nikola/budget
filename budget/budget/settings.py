fdc2f9755f5260964023e3058767f0aa6ceae886
georg-nikola <gn.pavlov96@gmail.com>
2022-04-12 18:50:12 +0300

    Changed DB to Postgres

--- budget/budget/settings.py
+++ budget/budget/settings.py
@@ -70,8 +70,12 @@ WSGI_APPLICATION = 'budget.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'budget_db',
+        'USER': 'postgres',
+        'PASSWORD': '1123QwER',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
