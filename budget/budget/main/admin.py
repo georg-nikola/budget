f6391dfbb3b3f14eb146b8d8999541bfcdcb2e95
georg-nikola <gn.pavlov96@gmail.com>
2022-04-12 21:54:10 +0300

    Customize admin

--- budget/budget/main/admin.py
+++ budget/budget/main/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from budget.main.models import Budget, Income, Expense
+
+
+class BudgetInlineAdmin(admin.StackedInline):
+    model = Budget
+
+
+class IncomeInlineAdmin(admin.StackedInline):
+    model = Budget
+
+
+class ExpenseInlineAdmin(admin.StackedInline):
+    model = Budget
+
+
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ('month', 'value', 'user')
+
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('month', 'value', 'user')
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('month', 'value', 'user')
