896af27168849b4668d7b2c5d30fbd34d23fec25
georg-nikola <gn.pavlov96@gmail.com>
2022-04-15 16:32:01 +0300

    Add users to admin site

--- budget/budget/auth_app/admin.py
+++ budget/budget/auth_app/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 from budget.auth_app.models import Profile
+
+UserModel = get_user_model()
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
+
+
+@admin.register(UserModel)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username',)
